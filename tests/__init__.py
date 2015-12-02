b6cab37535980b363778c782cb4c64935799c513
resteve <resteve@zikzakmedia.com>
2015-12-02 09:42:42 +0100

    FIX loading Tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,7 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
+# This file is part sale_opportunity_quote module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
-from .test import suite
+from .test_sale_opportunity_quote import suite
 
 __all__ = ['suite']
