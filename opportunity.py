637cd176905e2ec741503f298c14da5c6d46a64a
resteve <resteve@zikzakmedia.com>
2016-04-28 14:45:15 +0200

    Since 3.6 create_sale return object

--- opportunity.py
+++ opportunity.py
@@ -1,10 +1,8 @@
 #This file is part of sale_opportunity_quote module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
 from trytond.model import fields
-from trytond.pool import Pool, PoolMeta
-
+from trytond.pool import PoolMeta
 
 __all__ = ['SaleOpportunity', 'Sale']
 
@@ -16,10 +14,8 @@ class SaleOpportunity:
     quotes = fields.One2Many('sale.sale', 'opportunity', 'Sales')
 
     def create_sale(self):
-        Sale = Pool().get('sale.sale')
-
         sale = super(SaleOpportunity, self).create_sale()
-        Sale.write([sale], {'opportunity': self.id})
+        sale.opportunity = self
         return sale
 
 
